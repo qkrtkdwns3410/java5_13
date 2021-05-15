fc031f23fc330d073b0955ef7abd364594c525c1
psj <qkrtkdwns3410>
2021-05-15 14:34:18 +0900

    class

---
+++ src/com/javalec/frame/Main.java
@@ -0,0 +1,12 @@
+package com.javalec.frame;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        MakeFrame makeFrame = new MakeFrame();
+        makeFrame.setSize(new Dimension(500, 500));
+        makeFrame.setVisible(true);
+        
+    }
+}
---
+++ src/com/javalec/swing/MainClass.java
@@ -0,0 +1,10 @@
+package com.javalec.swing;
+
+public class MainClass {
+    public static void main(String[] args) {
+        MakeComp makeComp = new MakeComp();
+        makeComp.pack();
+        makeComp.setVisible(true);
+        
+    }
+}
